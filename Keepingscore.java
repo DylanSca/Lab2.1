24f8514bb97652cdfedce43b51dba37e8dc20ed3
Dylan <rockrz91919@gmail.com>
2025-09-04 21:53:24 -0400

    to question 37

---
+++ Keepingscore.java
@@ -0,0 +1,30 @@
+
+/**
+ * Write a description of class Keepingscore here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class Keepingscore
+{
+    // instance variables - replace the example below with your own
+    private int score;
+
+    /**
+     * Constructor for objects of class Keepingscore
+     */
+    public Keepingscore()
+    {
+        // initialise instance variables
+        this.score=score;
+    }
+
+    public int getScore(){
+        return score;
+    }
+    
+    public void increaseScore(){
+        score= score+1;
+    }
+    
+}
